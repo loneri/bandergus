a584444060c0ef0b71ff26dc5f5f7ad108219450
Anastasia Koptilova <koptilova.nastya@gmail.com>
2022-04-27 16:30:09 +0300

    Add files via upload

---
+++ scripts/form (1).js
@@ -0,0 +1,67 @@
+const MODAL_ACTIVE_CLASS_NAME = 'modal-active';
+
+const formModal = document.querySelector('#form-modal');
+const successModal = document.querySelector('#success-modal');
+const form = document.querySelector('#form');
+
+const openFormModalBtn = document.querySelector('#open-form-modal-btn');
+const launchBtn = document.querySelector('#launch-btn');
+const closeBtns = document.querySelectorAll('.close-btn');
+const closeBtnsGus = document.querySelectorAll('.close-btn-gus');
+
+openFormModalBtn.addEventListener('click', () => {
+    formModal.classList.add(MODAL_ACTIVE_CLASS_NAME);
+})
+
+const closeFormModal = () => {
+    formModal.classList.remove(MODAL_ACTIVE_CLASS_NAME);
+};
+
+const closeSuccessModal = () => {
+    successModal.classList.remove(MODAL_ACTIVE_CLASS_NAME);
+};
+
+const openSuccessModal = () => {
+    successModal.classList.add(MODAL_ACTIVE_CLASS_NAME);
+};
+
+closeBtns.forEach(btn => {
+    btn.addEventListener('click', e => {
+        e.stopPropagation();
+        closeFormModal();
+        closeSuccessModal();
+    })
+})
+
+closeBtnsGus.forEach(btn => {
+    btn.addEventListener('click', e => {
+        e.stopPropagation();
+        closeSuccessModal();
+    })
+})
+
+function clearFormFields() {
+    const modalFiends = formModal.querySelectorAll('input');
+
+    modalFiends.forEach( field => { 
+        field.value = ''
+    });
+}
+
+form.addEventListener('submit', e => {
+    e.preventDefault();
+    const formData = new FormData(form);
+
+    fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: new URLSearchParams(formData).toString(),
+    })
+      .then(() => {
+        closeFormModal();
+        setTimeout(openSuccessModal, 700);
+    
+      })
+      .catch((error) => console.log('Sending form failed'));
+})
+
